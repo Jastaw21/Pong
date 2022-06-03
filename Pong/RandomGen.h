dc3554af5139b1794d8530bca085ceec287a52c7
Jastaw21 <jacksweddell@outlook.com>
2022-06-03 10:28:08 +0100

    Beginnings of random number gen

---
+++ Pong/RandomGen.h
@@ -0,0 +1,19 @@
+#ifndef RANDOMGEN_H
+#define RANDOMGEN_H
+
+#include <chrono>
+#include <random>
+
+#include "Geometry.h"
+
+class RandomGen {
+  RandomGen(WindowGeom wg) : xrand{0, wg.WIDTH}, yrand{0, wg.HEIGHT} {}
+
+ private:
+  std::mt19937 twister{static_cast<unsigned int>(
+      std::chrono::steady_clock::now().time_since_epoch().count())};
+  std::uniform_int_distribution<> xrand{};
+  std::uniform_int_distribution<> yrand{};
+};
+
+#endif  // !RANDOMGEN_H
